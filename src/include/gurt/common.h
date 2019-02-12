dd8f247efe6267d30d832ee88c57f1c990833a59
Yulu Jia <yulu.jia@intel.com>
2019-02-11 16:56:51 -0800

    range of malloc test
    Change-Id: I80da0d6c0396c44aea648a18d9fdee9b488b8db7
    Signed-off-by: Yulu Jia <yulu.jia@intel.com>

--- src/include/gurt/common.h
+++ src/include/gurt/common.h
@@ -116,9 +116,22 @@ typedef struct {
 	d_iov_t		*sg_iovs;
 } d_sg_list_t;
 
+static bool
+is_on_stack(void *ptr)
+{
+	int local_var;
+
+	if ((uintptr_t)&local_var < (uintptr_t)ptr &&
+	    ((uintptr_t)&local_var + 100) > (uintptr_t)ptr)
+		return true;
+	return false;
+}
+
 static inline void
 d_iov_set(d_iov_t *iov, void *buf, size_t size)
 {
+	D_ASSERTF(!is_on_stack(buf), "buf (%p) is on the stack.\n", buf);
+
 	iov->iov_buf = buf;
 	iov->iov_len = iov->iov_buf_len = size;
 }
