920c68fff83be238da6b637867d9d45363aa9871
Sylvia Chan <sylvia.oi.yee.chan@intel.com>
2019-06-07 21:45:32 +0000

    CART-XXX Avocado Migration
    Enable five-node corpc tests
    
    Signed-off-by: Sylvia Chan <sylvia.oi.yee.chan@intel.com>

--- test/singleton/cart_singleton_one_node.py
+++ test/singleton/cart_singleton_one_node.py
@@ -82,7 +82,7 @@ class CartSingletonOneNodeTest(Test):
                                                          '/run/defaultENV/')
         os.environ["OFI_INTERFACE"] = self.params.get("OFI_INTERFACE",
                                                       '/run/defaultENV/')
-        os.environ["CRT_CTX_NUM"] = self.params.get("cli_CRT_CTX_NUM", 
+        os.environ["CRT_CTX_NUM"] = self.params.get("cli_CRT_CTX_NUM",
                                                     '/run/defaultENV/')
         os.environ["CRT_CTX_SHARE_ADDR"] = self.params.get("CRT_CTX_SHARE_ADDR",
                                                '/run/env_CRT_CTX_SHARE_ADDR/*/')
@@ -96,7 +96,7 @@ class CartSingletonOneNodeTest(Test):
         cli_rtn = self.utils.launch_cmd(self, clicmd)
 
         # Stop the server
-        print("Stopping server process {}".format(srv_rtn))        
+        print("Stopping server process {}".format(srv_rtn))
         procrtn = self.utils.stop_process(srv_rtn)
 
         if cli_rtn or procrtn:
