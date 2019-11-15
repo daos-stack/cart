2b325c8167d98bfdc04cf92445b4bd24a598afd2
Alexander A Oganezov <alexander.a.oganezov@intel.com>
2019-11-15 16:17:18 +0000

    Retrigger
    Signed-off-by: Alexander A Oganezov <alexander.a.oganezov@intel.com>

--- src/cart/crt_group.c
+++ src/cart/crt_group.c
@@ -3313,6 +3313,7 @@ crt_grp_config_psr_load(struct crt_grp_priv *grp_priv, d_rank_t psr_rank)
 	D_ALLOC(grpname, CRT_GROUP_ID_MAX_LEN + 1);
 	if (grpname == NULL)
 		D_GOTO(out, rc = -DER_NOMEM);
+
 	snprintf(fmt, 64, "%%*s%%%ds", CRT_GROUP_ID_MAX_LEN);
 	rc = fscanf(fp, fmt, grpname);
 	if (rc == EOF) {
