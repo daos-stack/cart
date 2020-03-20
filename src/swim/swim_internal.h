5dddd39665e4d0d51a6f11968a279661db862c3b
Dmitry Eremin <dmitry.eremin@intel.com>
2020-03-20 11:49:21 +0300

    copyrights update

--- src/swim/swim_internal.h
+++ src/swim/swim_internal.h
@@ -1,5 +1,5 @@
 /* Copyright (c) 2016 UChicago Argonne, LLC
- * Copyright (C) 2018-2019 Intel Corporation
+ * Copyright (C) 2018-2020 Intel Corporation
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
